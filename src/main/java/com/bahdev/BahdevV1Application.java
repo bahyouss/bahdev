f555ae5b5cbb788012e27dd077f58f4137c2faa3
bahyouss <byoussouf@gmail.com>
2018-09-15 09:55:26 -0600

    Initial commit for my first spring boot project

---
+++ src/main/java/com/bahdev/BahdevV1Application.java
@@ -0,0 +1,12 @@
+package com.bahdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BahdevV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BahdevV1Application.class, args);
+	}
+}
